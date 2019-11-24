307ae13a695150ca1c7f953c38a8bb4d8899c21f
Blue-Sky-r <robert.puskajler@yahoo.ca>
2019-11-24 21:34:17 +0100

    comments updated

--- bin/cli-flash.sh
+++ bin/cli-flash.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# CLI flash for TS100
-#
+# CLI tool for flashing TS100
+# Ubuntu 14.04.6 LTS + TDE R14.0.6
 
 [ $# -lt 1 ] && cat <<< """
 usage: $0 fw.hex
@@ -10,10 +10,21 @@ flash fimware fw.hex into TS100
 
 """ && exit 1
 
+# firmware
+#
 FW=$1
+
+# tmp mountpoint
+#
 TMPMOUNT='/tmp/ts100'
+
+# dst path to copy firmware to
+#
 #FWPATH="$TMPMOUNT/$( basename $FW | tr a-z A-Z )"
 FWPATH="$TMPMOUNT/$( basename $FW )"
+
+# flag - firmware successfuly updated
+#
 FWRDY=$( basename ${FW} '.hex' ).rdy
 
 #echo "fw($FW) FWPATH($FWPATH) FWRDY($FWRDY)"; exit
@@ -34,6 +45,7 @@ mkdir "$TMPMOUNT" 2>/dev/null
 
 while sleep 1;
 do
+    # NAME="sda" MODEL="WDC WD10EALS-00Z"
 	dfu=$( lsblk -P -d --output NAME,MODEL | grep "DFU Disk" )
 	[ ! -z "$dfu" ] && break
 	echo -n "."
@@ -42,9 +54,10 @@ done
 [ -z "$dfu" ] && echo "ERR: DFU disk not found" && exit 2
 
 echo "DFU mode found: $dfu"
+# NAME="sda" MODEL="WDC WD10EALS-00Z"
 eval $dfu
 
-# mount in MS-DOS mode	
+# mount DFU disk in MS-DOS mode
 #
 umount "/dev/$NAME" 2>/dev/null
 sudo mount -t msdos "/dev/$NAME" "$TMPMOUNT"
@@ -59,6 +72,8 @@ echo "done"
 sudo umount "/dev/$NAME"
 rmdir "$TMPMOUNT"
 
+# TS100 rebooting
+#
 echo -en "Waiting for TS100 reboot and automount "
 
 # reboot & automount
@@ -75,4 +90,5 @@ done
 # verity RDY file
 #
 rdy=$( find  "$mntdir/" -iname "$FWRDY" ) 
+echo; echo
 [ -z "$rdy" ] && echo "ERROR flashing firmware $FW" || echo "Firmwre $FW flashed OK ($rdy)"
